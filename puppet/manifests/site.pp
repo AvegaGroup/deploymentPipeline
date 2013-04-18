46417d168feeffaf0e6b03548d8bb5b008eea6ea
Mikael Sennerholm <mikael@sennerholm.net>
2013-04-18 21:04:16 +0200

    Uppdates for razor and some more stuff

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,11 @@
 # site.pp 
+# Inspired by
+# http://projects.puppetlabs.com/projects/puppet/wiki/Advanced_Puppet_Pattern
 # define nodes
 import "nodes.pp"
+
+# The filebucket option allows for file backups to the server
+filebucket { main: server => 'puppetmaster.hem.sennerholm.net' }
+
+# Set global defaults - including backing up all files to the main filebucket and adds a global path
+File { backup => main }
