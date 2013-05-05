748ce600ba9da398f3b48197dd2038c0574b4f0b
Mikael Sennerholms computer <mikael@sennerholm.net>
2013-05-05 21:25:32 +0200

    Basic configuration of prod and test server with local mysql and tomcat

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -20,6 +20,44 @@ node basenode {
 node default inherits basenode {    
 }
 
+node /prod/ inherits basenode {
+  package { 'mysql-server' :
+    ensure  => present,
+    provider=> 'apt'
+  }
+  package { 'openjdk-7-jre' :
+    ensure  => present,
+    provider=> 'apt'
+  }
+  package { 'openjdk-6-jre-headless' :
+    ensure  => purged,
+    provider=> 'apt'
+  } 
+  
+  package { 'tomcat7' :
+    ensure  => present,
+    provider=> 'apt'
+  }
+}
+
+node /test/ inherits basenode {
+  package { 'mysql-server' :
+    ensure  => present,
+    provider=> 'apt'
+  }
+  package { 'openjdk-7-jre' :
+    ensure  => present,
+    provider=> 'apt'
+  }
+  package { 'openjdk-6-jre-headless' :
+    ensure  => purged,
+    provider=> 'apt'
+  }
+  package { 'tomcat7' :
+    ensure  => present,
+    provider=> 'apt'
+  }
+}
 
 import "puppetmaster.pp"
 
