87528b4cd52bd7bdf701ee9e7022e8c97b339697
Daniel Fagerstrom <daniel@danielfagerstrom.com>
2013-05-11 12:29:33 +0200

    Ensure that the local apt repository is updated.

--- puppet/install-modules.sh
+++ puppet/install-modules.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+# An update will will be needed to be able to install most of the packages, somewhat kludgy
+# to put it here, but letting everything in puppet to depend on a update execute would be worse.
+sudo apt-get update
+
 MODULE_DIR=/etc/puppet/modules
 [ ! -d $MODULE_DIR ] && mkdir -p $MODULE_DIR
 
