9261323d7f260e43a7b06fa80023405576efb7fd
Mikael Sennerholm <mikael@sennerholm.net>
2013-06-17 18:45:45 +0200

    Install hardcoded hosts files to resolve names, no fqdn yet.

---
+++ vagrant/install-modules.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# Ensure that we have the correct host file for all hosts
+if [ /vagrant/vagrant/hosts -nt /etc/hosts  ]; 
+then  
+    cp /vagrant/vagrant/hosts /etc/hosts ;
+    chown root:root /etc/hosts; 
+fi
+
+
+# An update will will be needed to be able to install most of the packages, somewhat kludgy
+# to put it here, but letting everything in puppet to depend on a update execute would be worse.
+sudo apt-get update
+
+MODULE_DIR=/etc/puppet/modules
+[ ! -d $MODULE_DIR ] && mkdir -p $MODULE_DIR
+
+[ ! -d $MODULE_DIR/apt ] && puppet module install puppetlabs/apt
+[ ! -d $MODULE_DIR/jenkins ] && puppet module install rtyler/jenkins
+[ ! -d $MODULE_DIR/ntp ] && puppet module install puppetlabs/ntp 
+[ ! -d $MODULE_DIR/timezone ] && puppet module install saz/timezone
+[ ! -d $MODULE_DIR/sudo ] && puppet module install saz/sudo
+
+
+exit 0
+
