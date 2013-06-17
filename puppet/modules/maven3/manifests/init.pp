8ad4b3567f45fa8ae4bf6543f7e6307c26bb2127
Mikael Sennerholm <mikael@sennerholm.net>
2013-06-17 20:19:08 +0200

    Changed user

--- puppet/modules/maven3/manifests/init.pp
+++ puppet/modules/maven3/manifests/init.pp
@@ -12,9 +12,9 @@ class maven3 {
     }
 # copy a remote file to /etc/sudoers
     file { "/usr/share/maven3/conf/settings.xml":
-      mode => 440,
-      owner => root,
-      group => root,
+      mode => 644,
+      owner => jenkins,
+      group => jenkins,
       source => "puppet:///modules/maven3/settings.xml"
     }
 }
