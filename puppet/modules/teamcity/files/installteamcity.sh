a12a142836ad7f0b44f0132ec78eea4d8b88a9ee
Johan Elmström <johan.elmstrom@gmail.com>
2014-02-11 15:35:28 +0100

    Install and start teamcity server

--- puppet/modules/teamcity/files/installteamcity.sh
+++ puppet/modules/teamcity/files/installteamcity.sh
@@ -1,8 +1,12 @@
+echo "downloading TeamCity"
+sudo wget -c http://download.jetbrains.com/teamcity/TeamCity-8.0.6.tar.gz
 
-
+echo "Untar"
 tar -xf TeamCity-8.0.6.tar.gz
 chown -R vagrant /opt/TeamCity
 
+echo "modify init.d"
 sudo update-rc.d teamcity defaults
 /etc/init.d/teamcity start
 
+return 0
