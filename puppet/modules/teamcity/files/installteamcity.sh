362a2a5c94e267abc6f61f369a71a4d3fce1eedf
Johan Elmström <johan.elmstrom@gmail.com>
2014-02-18 15:37:07 +0100

    move teamcity config to separate node

--- puppet/modules/teamcity/files/installteamcity.sh
+++ puppet/modules/teamcity/files/installteamcity.sh
@@ -5,6 +5,14 @@ echo "Untar"
 tar -xf TeamCity-8.0.6.tar.gz
 chown -R vagrant /opt/TeamCity
 
+sudo wget http://repo1.maven.org/maven2/mysql/mysql-connector-java/5.1.28/mysql-connector-java-5.1.28.jar
+sudo chown -R vagrant mysql-connector-java-5.1.28.jar
+sudo chgrp -R vagrant mysql-connector-java-5.1.28.jar
+
+mkdir TeamCity/.BuildServer/lib/jdbc
+sudo mv mysql-connector-java-5.1.28.jar TeamCity/.BuildServer/lib/jdbc
+
+
 echo "modify init.d"
 sudo update-rc.d teamcity defaults
 /etc/init.d/teamcity start
