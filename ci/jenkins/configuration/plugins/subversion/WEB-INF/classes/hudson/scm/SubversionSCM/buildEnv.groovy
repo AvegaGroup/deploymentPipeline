433298544fa0840b923e33d2d9013248c7d3a867
oGre <oGre@muppfarmen.se>
2014-02-18 21:08:50 +0100

    Removed old unused configs

--- ci/jenkins/configuration/plugins/subversion/WEB-INF/classes/hudson/scm/SubversionSCM/buildEnv.groovy
+++
@@ -1,9 +0,0 @@
-package hudson.scm.SubversionSCM;
-
-def l = namespace(lib.JenkinsTagLib)
-
-["SVN_REVISION","SVN_URL"].each { name ->
-    l.buildEnvVar(name:name) {
-        raw(_("${name}.blurb"))
-    }
-}
