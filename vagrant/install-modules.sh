0267b7f1a48ed3a85760c14e331d04db63460596
Johan Elmström <johan.elmstrom@gmail.com>
2014-03-05 09:07:13 +0100

    set fixed version for jenkins

--- vagrant/install-modules.sh
+++ vagrant/install-modules.sh
@@ -16,7 +16,7 @@ MODULE_DIR=/etc/puppet/modules
 [ ! -d $MODULE_DIR ] && mkdir -p $MODULE_DIR
 
 [ ! -d $MODULE_DIR/apt ] && puppet module install puppetlabs/apt
-[ ! -d $MODULE_DIR/jenkins ] && puppet module install rtyler/jenkins
+[ ! -d $MODULE_DIR/jenkins ] && puppet module install rtyler/jenkins --version 0.3.1
 [ ! -d $MODULE_DIR/ntp ] && puppet module install puppetlabs/ntp 
 [ ! -d $MODULE_DIR/timezone ] && puppet module install saz/timezone
 [ ! -d $MODULE_DIR/sudo ] && puppet module install saz/sudo
